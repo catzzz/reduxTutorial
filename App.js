fbcb601357fbae9acb5d3f9f6e260e67140090dc
Jimmy Leu <jimmyleu@Jimmyde-MBP.hsd1.ca.comcast.net>
2019-05-16 00:23:55 -0700

    finish basic

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import CounterApp from './src/CounterApp'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+
+
+const initialState = {
+  counter:0
+}
+
+const reducer = (state = initialState,action)=>{
+  switch (action.type)
+  {
+    case'INCREASE_COUNTER':
+    return {counter:state.counter+1}
+    case'DECREASE_COUNTER':
+    return {counter:state.counter-1}
+  }
+  return state
+}
+
+const store = createStore(reducer)
+
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+
+
+  render() {
+    return (
+      <Provider store = {store}>
+        <CounterApp/>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+});
